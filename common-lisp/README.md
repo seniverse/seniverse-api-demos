bc645a7486b02ed2dbe426ab2f0c34b150aca2d2
chenwei <chenwei@huobi.com>
2017-02-14 16:33:11 +0800

    Add Common Lisp demo.

---
+++ common-lisp/README.md
@@ -0,0 +1,33 @@
+# Common Lisp 调用 API 示例
+使用库 `drakma` 发送 HTTP 请求进行 API 的调用
+
+##使用更安全的签名验证方式（荐）
+使用以下库进行签名：
+* cl-base64
+* ironclad
+* flexi-streams
+
+###Requirements
+* CL implementation installed(eg. SBCL, CCL or LispWorks)
+* Quicklisp installed
+
+###Prepare
+```shell
+$ git clone https://github.com/thinkpage/api-demos.git
+$ ln -s api-demos/common-lisp/ ~/quicklisp/local-projects/thinkpage-demo
+```
+
+###Run
+```shell
+$ sbcl
+```
+注：这里使用 [SBCL](http://sbcl.org) 作为运行环境
+
+```lisp
+(ql:quickload 'thinkpage-demo)
+```
+示例输出结果：
+```
+{"results":[{"location":{"id":"WX4FBXXFKE4F","name":"北京","country":"CN","path":"北京,北京,中国","timezone":"Asia/Shanghai","timezone_offset":"+08:00"},"now":{"text":"多云","code":"4","temperature":"4","feels_like":"3","pressure":"1024","humidity":"53","visibility":"2.0","wind_direction":"东南","wind_direction_degree":"117","wind_speed":"3.6","wind_scale":"1","clouds":"","dew_point":""},"last_update":"2017-02-14T15:45:00+08:00"}]}
+200
+```
---
+++ common-lisp/thinkpage-demo.lisp
@@ -0,0 +1,50 @@
+(defpackage thinkpage-demo
+  (:use :cl))
+
+(in-package :thinkpage-demo)
+
+(defvar *api-key* "4r9bergjetiv1tsd") ;; Please use your own API key
+(defvar *user-id* "U785B76FC9")       ;; Please use your own User ID
+(defvar *auth-ttl* 1800) ;; second
+(defvar *api-url* "https://api.thinkpage.cn/v3/weather/now.json")
+(defvar *universal-time-diff-unix-time* 2208988800)
+
+(defun universal-time->unix-time (universal-time)
+  (- universal-time *universal-time-diff-unix-time*))
+
+(defun unix-time->universal-time (unix-time)
+  (+ unix-time *universal-time-diff-unix-time*))
+
+(defun test ()
+  (let* ((unix-time (universal-time->unix-time (get-universal-time)))
+         (string (format nil "ts=~A&ttl=~A&uid=~A"
+                         unix-time
+                         *auth-ttl*
+                         *user-id*))
+         (string-vector (flex:string-to-octets string :external-format :utf-8))
+         (key-vector (flex:string-to-octets *api-key* :external-format :utf-8))
+         (hmac (ironclad:make-hmac key-vector 'ironclad:sha1))
+         (signature (progn
+                      (ironclad:update-hmac hmac string-vector)
+                      (drakma:url-encode 
+                       (base64:usb8-array-to-base64-string
+                        (ironclad:hmac-digest hmac))
+                       :utf-8)))
+         (request-url (format nil "~A?location=~A&ts=~A&ttl=~A&uid=~A&sig=~A"
+                              *api-url*
+                              "beijing"
+                              unix-time
+                              *auth-ttl*
+                              *user-id*
+                              signature)))
+    (setf request-url (ppcre:regex-replace-all "%3D" request-url "%3d"))
+    (format *terminal-io* "request-url: ~A~%~%" request-url)
+    (multiple-value-bind (data status-code)
+        (drakma:http-request request-url
+                             :preserve-uri t
+                             :additional-headers '(("Accept-Encoding" . "")))
+      (format *terminal-io* "~A~%~A~%"
+              (flex:octets-to-string data :external-format :utf-8)
+              status-code))))
+    
+(test)
