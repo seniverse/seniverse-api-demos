2e3e868c69f09e15d6c706c62c42a12625a14dfa
meng <dance.jms@gmail.com>
2017-04-12 19:44:10 +0800

    移除对第三方库 utils 的依赖

--- python/demo-requests.py
+++ python/demo-requests.py
@@ -1,20 +1,22 @@
-import json
-import sys
 import requests
-from utils.const_value import API, KEY, UNIT, LANGUAGE
-from utils.helper import getLocation
+'''
+运行前请先更改 payload 中键 'key'、'location'所对应的值
+'''
 
+#获取指定城市天气实况的 api 地址
+api_url = 'https://api.seniverse.com/v3/weather/now.json'
 
-def fetchWeather(location):
-    result = requests.get(API, params={
-        'key': KEY,
-        'location': location,
-        'language': LANGUAGE,
-        'unit': UNIT
-    }, timeout=1)
+payload = {
+    'key' : '你的key值', #你注册后得到的 key 值，在此处查看 https://www.seniverse.com/doc#info
+    'location' : '你要查询的城市',  #城市的拼音如 "beijing" 或直接传入"北京"
+    'language' : 'zh-Hans',
+    'unit' :'c',
+}
+
+def fetchWeather():
+    result = requests.get(api_url, params=payload, timeout=1)
     return result.text
 
 if __name__ == '__main__':
-    location = getLocation()
-    result = fetchWeather(location)
+    result = fetchWeather()
     print(result)
